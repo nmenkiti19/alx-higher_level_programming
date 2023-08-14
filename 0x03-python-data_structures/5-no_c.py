2c0adb0dd8f921cba2269ba45ca660e42c3f7e12
nmenkiti19 <ngomenkiti@gmail.com>
2023-08-14 11:26:17 -0700

    fixed task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = [letter for letter in my_string if letter != 'c' and letter != 'C']
-    return ("".join(result))
+    res = [lt for lt in my_string if lt != 'c' and lt != 'C']
+    return ("".join(res))
